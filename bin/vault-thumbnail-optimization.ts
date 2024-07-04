3900aca0c46285a20d780a90c8cc9ad4cb193dcc
KarthickEka <Karthick@eka.care>
2024-07-04 12:03:14 +0530

    prod-update

--- bin/vault-thumbnail-optimization.ts
+++ bin/vault-thumbnail-optimization.ts
@@ -2,10 +2,18 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ImageOptimizationStack } from '../lib/vault-thumbnail-optimization';
-
+// import { ImageOptimizationProdStack } from '../lib/vault-thumbnail-optimization-prod'; // Assuming you have a separate stack for prod
 
 const app = new cdk.App();
-new ImageOptimizationStack(app, 'vault-thumbnail-optimization', {
+const env = app.node.tryGetContext('env');
 
-});
-
+if (env === 'prod') {
+    console.log(`testing`)
+//   new ImageOptimizationProdStack(app, 'vault-thumbnail-optimization-prod', {
+//     // Production-specific props
+//   });
+} else {
+  new ImageOptimizationStack(app, 'vault-thumbnail-optimization', {
+    // Development-specific props
+  });
+}
